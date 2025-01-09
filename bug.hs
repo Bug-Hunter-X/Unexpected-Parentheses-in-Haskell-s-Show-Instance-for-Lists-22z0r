89ba8f9e60a94ebf1a123401b4909c158bb7cb49
BugHunter-X <askzenai@gmail.com>
2025-01-09 12:40:13 +0100

    Add bug.hs file

---
+++ bug.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Main where
+
+class Show a where
+  showsPrec :: Int -> a -> ShowS
+
+instance Show a => Show [a] where
+  showsPrec p xs = showParen (p > 10) $ showList xs
+    where
+      showList []     = showString "[]"
+      showList [x]    = showsPrec 11 x
+      showList (x:xs) = showsPrec 11 x . showString "," . showList xs
+
+data MyData = MyData { x :: Int }
+
+instance Show MyData where
+  showsPrec p (MyData x) = showString "MyData {" . showsPrec 11 x . showString "}"
+
+main :: IO ()
+main = do
+  print [MyData {x = 1}, MyData {x = 2}]
